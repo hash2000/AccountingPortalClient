cb05e0d7de940805dd4b467f6a3a2babd7d74d78
Hash_2000 <sobchik.va@noyabrsk-dobycha.gazprom.ru>
2021-08-16 13:40:53 +0500

    XMLView load

--- ErpProj.Clients/Home/index-dbg.js
+++ ErpProj.Clients/Home/index-dbg.js
@@ -1,6 +1,21 @@
 
-sap.ui.getCore().attachInit(function () {
-    // new sap.ui.core.ComponentContainer({
-    //     name: "ErpProj.Home.Views.App"
-    // }).placeAt("content");
+// sap.ui.getCore().attachInit(function () {
+//     // new sap.ui.core.ComponentContainer({
+//     //     name: "ErpProj.Home.Views.App"
+//     // }).placeAt("content");
+// });
+
+
+sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+    "use strict";
+
+    XMLView.create({
+        viewName: "ErpProj.Home.Views.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    });
+
+
 });
