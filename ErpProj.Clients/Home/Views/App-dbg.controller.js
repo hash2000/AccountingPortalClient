96b7cda516d3afe3f738fe5aa4ded1f43464a2b3
Hash_2000 <sobchik.va@noyabrsk-dobycha.gazprom.ru>
2021-08-17 13:15:58 +0500

    HelloPanel Component

--- ErpProj.Clients/Home/Views/App-dbg.controller.js
+++ ErpProj.Clients/Home/Views/App-dbg.controller.js
@@ -4,13 +4,5 @@ sap.ui.define([
 ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("ErpProj.Home.Views.App", {
-        onShowHello: function () {
-            // read msg from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            // show message
-            MessageToast.show(sMsg);
-        }
     });
 });
