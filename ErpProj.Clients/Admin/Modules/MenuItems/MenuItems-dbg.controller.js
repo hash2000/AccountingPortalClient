704496ca26fc5eff22011d7a02013714ac49938c
Hash_2000 <sobchik.va@noyabrsk-dobycha.gazprom.ru>
2021-08-23 09:31:18 +0500

    заготовка для страниц "Узлы меню" и "Группы и роли"

---
+++ ErpProj.Clients/Admin/Modules/MenuItems/MenuItems-dbg.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    'sap/ui/Device',
+    'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel',
+    'sap/m/Popover',
+    'sap/m/Button',
+    'sap/m/library'
+], function (Device, Controller, JSONModel, Popover, Button, mobileLibrary) {
+    "use strict";
+    return Controller.extend("ErpProj.Modules.MenuItems.MenuItems", {
+
+
+
+    });
+});
