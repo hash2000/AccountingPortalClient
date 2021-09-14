cc8bf39bfa5f2e89545971b3d368d7ad89e24fc5
Hash_2000 <sobchik.va@noyabrsk-dobycha.gazprom.ru>
2021-09-14 15:18:43 +0500

    LoginDialog

--- ErpProj.Clients/Admin/Views/App-dbg.controller.js
+++ ErpProj.Clients/Admin/Views/App-dbg.controller.js
@@ -4,13 +4,15 @@ sap.ui.define([
     'sap/ui/model/json/JSONModel',
     'sap/m/Popover',
     'sap/m/Button',
-    'sap/m/library'
-], function (Device, Controller, JSONModel, Popover, Button, library) {
+    'sap/m/library',
+    'sap/ui/core/Fragment',
+    'sap/ui/model/odata/v2/ODataModel'
+], function (Device, Controller, JSONModel, Popover, Button, library, Fragment, ODataModel) {
     "use strict";
 
     var ButtonType = library.ButtonType,
         PlacementType = library.PlacementType;
-    
+
     return Controller.extend("ErpProj.Home.Views.App", {
 
         onInit: function () {
@@ -19,20 +21,62 @@ sap.ui.define([
             this.getView().setModel(this.oModel);
         },
 
+        onLoginButtonOkPress: function (event) {
+            var me = this;
+            me._loginDialogFragment.then(function (oDialog) {
+                var oModel = oDialog.getModel(),
+                    oData = oModel.getData();
+                
+                // sap.ui.require.toUrl("localhost:6001/security/admin/auth/");
+                
+                oDialog.close();
+            });
+        },
+
+        onLoginButtonCancelPress: function (event) {
+            this._loginDialogFragment.then(function (oDialog) {
+                oDialog.close();
+            });
+        },
+
         onAuthorizationPress: function (event) {
+            var buttons = [];
+            var me = this;
+            var oView = me.getView();
+            
+            buttons.push(new Button({
+                text: 'Вход',
+                type: ButtonType.Transparent,
+                press: function (event) {
+                    if (!me._loginDialogFragment) {
+                        me._loginDialogFragment = Fragment.load({
+                            // id: oView.getId(),
+                            name: 'ErpProj.Home.Views.LoginDialog',
+                            controller: me
+                        }).then(function (oDialog) {
+                            oView.addDependent(oDialog);
+                            return oDialog;
+                        });
+                    }
+
+                    me._loginDialogFragment.then(function (oDialog) {
+                        var oModel = new JSONModel({
+                            username: "",
+                            password: ""
+                        });
+                        oDialog.setModel(oModel);
+                        oDialog.open();
+                    });
+                }
+            }));
+
             var oPopover = new Popover({
                 showHeader: false,
                 placement: PlacementType.Bottom,
-                content: [
-                    new Button({
-                        text: 'Вход',
-                        type: ButtonType.Transparent
-                    })
-                ]
+                content: buttons
             }).addStyleClass('sapMOTAPopover sapTntToolHeaderPopover');
 
             var oSender = event.getSource();
-
 
             oPopover.openBy(oSender);
         },
@@ -44,7 +88,7 @@ sap.ui.define([
                 router = owner.getRouter();
             router.navTo(itemKey);
         },
-        
+
         onMenuButtonPress: function () {
             var toolPage = this.byId("toolPage");
             toolPage.setSideExpanded(!toolPage.getSideExpanded());
